7519400e908224cf9994004663546879cf34ced3
Nikita Peplov <npeplov@yandex.ru>
2020-10-15 13:36:16 +0300

    Changed .className= -> .classList.methods. Fixed overlay

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -2,7 +2,7 @@ const modalMain = document.getElementById("modal_main");
 const modalSuccess = document.getElementById("modal_success");
 
 // Отобразить окно при старте
-modalMain.className="modal modal_active";
+modalMain.classList.add("modal_active");
 
 const closeButton = document.getElementsByClassName("modal__close modal__close_times");
 const btnSuccess = document.getElementsByClassName("btn btn_success");
@@ -10,12 +10,14 @@ const btnSuccess = document.getElementsByClassName("btn btn_success");
 btnSuccess[0].onclick =
 closeButton[0].onclick =
 closeButton[1].onclick = () => {
-    modalMain.className="modal";
-    modalSuccess.className="modal";
+    modalMain.classList.remove("modal_active");
+    modalSuccess.classList.remove("modal_active");
 }
 
-const showButton = document.getElementsByClassName("btn btn_danger modal__close show-success")
+const showButton = document.getElementsByClassName("btn btn_danger modal__close show-success");
 
 showButton[0].onclick = () => {
-    document.getElementById('modal_success').className="modal modal_active";
+    // Закрыть первое окно с id="modal_main"
+    modalMain.classList.remove("modal_active");
+    document.getElementById('modal_success').classList.add("modal_active");
 }
