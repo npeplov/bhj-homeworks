4e1234fc56acc6dce3838086b4b9507936be1cdc
Nikita Peplov <npeplov@yandex.ru>
2020-10-31 12:35:34 +0300

    1th task. Added toggle class 'tooltip_active'

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -2,7 +2,7 @@
 const aArr = Array.from(document.querySelectorAll('a.has-tooltip'));
 
 let tooltip = document.createElement('div');
-tooltip.className = 'tooltip tooltip_active';
+tooltip.className = 'tooltip';
 
 aArr.forEach( (a) => {
     a.addEventListener('click', showTooltip)
@@ -10,24 +10,12 @@ aArr.forEach( (a) => {
 );
 
 function showTooltip(event) {
+    tooltip.classList.toggle('tooltip_active');
     tooltip.style.left = this.offsetLeft + 'px';
     tooltip.style.top = this.getBoundingClientRect().top + 20 + 'px';
-
-    // setAttribute method:
-    // tooltip.setAttribute('style', `left: ${this.getBoundingClientRect().left + 'px'};`)
 
     event.preventDefault();
 
     tooltip.innerText = this.title;
     this.appendChild(tooltip);
 }
-
-
-
-// неправильное положение подсказки по горизонтали
-// 1. подсказки в начале задания
-// https://github.com/netology-code/bhj-homeworks/tree/master/document-structure
-
-// 2. написать разметку самому
-// <a>1</a> <div>2</div>
-// и сделать с помощью css
