983ed341e405178461b98db43ae8ef901f20be82
Nikita Peplov <npeplov@yandex.ru>
2020-11-01 10:22:00 +0300

    1th task. Origin

---
+++ client-state/text-editor/task.js
@@ -0,0 +1,18 @@
+const text = document.querySelector('textarea');
+const clearButton = document.querySelector('button');
+
+document.addEventListener('DOMContentLoaded', load);
+text.addEventListener('input', save);
+clearButton.addEventListener('click', clear)
+
+function load() {
+    text.value = localStorage.getItem('text-editor', text.value);
+}
+
+function save() {
+    localStorage.setItem('text-editor', (text.value));
+}
+
+function clear() {
+    text.value = '';
+}
